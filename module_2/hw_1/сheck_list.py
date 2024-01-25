dca065aee27556e7fc5cf9d895878097e5bf0be0
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-01-25 13:13:17 +0200

    homework 1 finished

---
+++ module_2/hw_1/сheck_list.py
@@ -0,0 +1,8 @@
+my_list = ["apple", "pear", "orange", "banana"]
+
+element_to_check = "apple"
+
+if element_to_check in my_list:
+    print(f"Yes, element '{element_to_check}' exists.")
+else:
+    print(f"No, element '{element_to_check}' does not exist.")
