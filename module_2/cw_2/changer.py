4541934eb07de899dd9a44e43e7cef356df09155
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-01-28 12:32:56 +0200

    test auto change

--- module_2/cw_2/changer.py
+++ module_2/cw_2/changer.py
@@ -1,20 +1,38 @@
-from pathlib import Path
-from git import Repo
+import os
 
-home_dir = Path.home()
-project_dir = Path.home() / "PycharmProjects" / "hw_devops" / "module_2" / "cw_2" / "project"
+file_path = "/home/tysser/PycharmProjects/hw_devops/module_2/cw_2/project/variable.tf"
 
-print(project_dir)
-repo_url = "git@github.com:yourhostel/terraform.git"
+new_value = 'm5.large'
 
+with open(file_path, 'r') as file:
+    file_data = file.read()
 
-def clone_repo():
-    local_path = project_dir
-    try:
-        Repo.clone_from(repo_url, local_path)
-        print(f"Git repository cloned successfully in {local_path}")
-    except Exception as e:
-        print(f"Error with cloning repo: {e}")
+new_file_data = file_data.replace('default     = "t2.micro"', f'default     = "{new_value}"')
+
+with open(file_path, 'w') as file:
+    file.write(new_file_data)
+
+print(f'Value in variable.tf has been changed to {new_value}')
 
 
-clone_repo()
+
+# from pathlib import Path
+# from git import Repo
+#
+# home_dir = Path.home()
+# project_dir = Path.home() / "PycharmProjects" / "hw_devops" / "module_2" / "cw_2" / "project"
+#
+# print(project_dir)
+# repo_url = "git@github.com:yourhostel/terraform.git"
+#
+#
+# def clone_repo():
+#     local_path = project_dir
+#     try:
+#         Repo.clone_from(repo_url, local_path)
+#         print(f"Git repository cloned successfully in {local_path}")
+#     except Exception as e:
+#         print(f"Error with cloning repo: {e}")
+#
+#
+# clone_repo()
