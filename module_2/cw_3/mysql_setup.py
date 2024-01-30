cab4b55fa7a2d8cd03f420b609ac645a0c43dd67
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-01-30 20:46:26 +0200

    corrected

--- module_2/cw_3/mysql_setup.py
+++ module_2/cw_3/mysql_setup.py
@@ -5,9 +5,32 @@ import string
 import getpass
 
 
+def install_mysql():
+    # Перевірка, чи встановлено MySQL
+    if not subprocess.run(["dpkg-query", "-l", "mysql-server"], capture_output=True, text=True).stdout:
+        # Встановлення MySQL, якщо він не встановлений
+        subprocess.run(["sudo", "apt", "install", "-y", "mysql-server"])
+        print("MySQL Server встановлено.")
+
+        # Після встановлення намагаємося запустити MySQL
+        subprocess.run(["sudo", "systemctl", "start", "mysql"])
+        print("Сервіс MySQL запущено.")
+    else:
+        print("MySQL Server вже встановлено.")
+
+        # Перевірка, чи запущено MySQL
+        status = subprocess.run(["systemctl", "is-active", "mysql"], capture_output=True, text=True).stdout.strip()
+        if status != "active":
+            subprocess.run(["sudo", "systemctl", "start", "mysql"])
+            print("Сервіс MySQL був неактивний і тепер запущено.")
+        else:
+            print("Сервіс MySQL вже запущено.")
+
+
 def install_mysql_connector():
+    # Перевірка та встановлення MySQL Connector/Python, якщо він не встановлений
     try:
-        import mysql_setup.connector
+        import mysql.connector
         print("MySQL Connector/Python вже встановлено.")
     except ImportError:
         print("Встановлення MySQL Connector/Python.")
@@ -16,11 +39,13 @@ def install_mysql_connector():
 
 
 def generate_password(length=12):
+    # Генерація випадкового пароля
     characters = string.ascii_letters + string.digits + string.punctuation
     return ''.join(random.choice(characters) for i in range(length))
 
 
 def create_mysql_user(user, password, root_password):
+    # Створення користувача MySQL
     try:
         import mysql.connector
         connection = mysql.connector.connect(
@@ -36,15 +61,18 @@ def create_mysql_user(user, password, root_password):
     except mysql.connector.Error as err:
         print("Помилка MySQL:", err)
     finally:
-        if connection.is_connected():
+        if connection and connection.is_connected():
             cursor.close()
             connection.close()
 
 
+# Встановлення MySQL Server, якщо він не встановлений
+install_mysql()
+
 # Встановлення MySQL Connector/Python, якщо він не встановлений
 install_mysql_connector()
 
-# Просимо користувача ввести пароль для root
+# Запит пароля для користувача root
 root_password = getpass.getpass("Будь ласка, введіть пароль для користувача root MySQL: ")
 
 # Генерація пароля
