7e4858214518ddc0875f2cbe1a07bb3b941316d5
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-01 21:03:55 +0200

    added last 10 logs

--- module_2/docker/run_nginx_docker.py
+++ module_2/docker/run_nginx_docker.py
@@ -4,8 +4,8 @@ client = docker.from_env()
 
 container = client.containers.run("nginx", detach=True, ports={'80/tcp': 8080})
 
-print(f"Nginx ID: {container.id}")
-print("Access Nginx http://localhost:8080")
+print(f"Nginx контейнер запущен с ID: {container.id}")
+print("Доступ к Nginx можно получить через http://localhost:8080")
 
-logs = container.logs()
+logs = container.logs(tail=10)
 print(logs.decode("utf-8"))
