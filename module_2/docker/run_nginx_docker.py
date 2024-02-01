02a7172bf5b560813d4c9a515507c73f3dbd7d6e
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-01 21:39:45 +0200

    added user Test

--- module_2/docker/run_nginx_docker.py
+++ module_2/docker/run_nginx_docker.py
@@ -5,8 +5,13 @@ client = docker.from_env()
 
 try:
     # Запускаємо контейнер Nginx, відображаючи порт 80 контейнера на порт 8080 хоста
-    container = client.containers.run("nginx:latest", detach=True, ports={"80/tcp": 8080})
-    print(f"Nginx running, ID: {container.id}")
-    print("Access in Nginx http://localhost:8080")
+    container = client.containers.run(
+        "nginx:latest",  # Назва образу
+        detach=True,  # Запуск в режимі фону
+        ports={"80/tcp": 8080},  # Відображення портів
+        user="Test"  # Вказуємо користувача для контейнера
+    )
+    print(f"Контейнер Nginx запущено, ID: {container.id}")
+    print("Доступ до Nginx можна отримати за адресою http://localhost:8080")
 except Exception as e:
     print(f"Помилка при запуску контейнера Nginx: {e}")
