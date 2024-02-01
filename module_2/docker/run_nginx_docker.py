ec7d671a0081d457d8d3a632adb7cab4adc6a5d5
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-01 21:12:58 +0200

    added other exception

--- module_2/docker/run_nginx_docker.py
+++ module_2/docker/run_nginx_docker.py
@@ -1,11 +1,17 @@
 import docker
+from docker.errors import ImageNotFound, APIError
 
 client = docker.from_env()
 
-container = client.containers.run("nginx", detach=True, ports={'80/tcp': 8080})
-
-print(f"Nginx контейнер запущен с ID: {container.id}")
-print("Доступ к Nginx можно получить через http://localhost:8080")
-
-logs = container.logs(tail=10)
-print(logs.decode("utf-8"))
+try:
+    container = client.containers.run("nginx", detach=True, ports={'80/tcp': 8080})
+    print(f"Nginx контейнер запущено с ID: {container.id}")
+    print("Доступ до Nginx можна отримати через http://localhost:8080")
+    logs = container.logs(tail=10)
+    print(logs.decode("utf-8"))
+except ImageNotFound:
+    print("Образ 'nginx:latest' не знайдений. Завантаження...")
+except APIError as e:
+    print(f"Виникла помилка Docker API: {e}")
+except Exception as e:
+    print(f"невідома помилка: {e}")
