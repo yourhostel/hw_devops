c4c6b9fdb417e781737b1c072f160306f8d7a957
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-01 16:41:11 +0200

    added прикольні_фічі_пайтон

---
+++ module_2/прикольні_фічі_пайтон/generator.py
@@ -0,0 +1,49 @@
+def create_counter_1(): # генерація на замиканні
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        print(count)
+    return increment
+
+
+counter_1 = create_counter_1()
+counter_1()
+
+def create_counter_2(): # генерація за допомогою функції-генератора
+    count = 0
+    while True:
+        count += 1
+        yield count
+
+counter_2 = create_counter_2()
+next(counter_2)
+
+def create_counter_3(): # крутий варіант через рекурсію
+    count = 0
+    def increment():
+        nonlocal count
+        count += 1
+        print(count)
+        return increment
+    return increment
+
+counter_3 = create_counter_3()
+
+counter_3()()
+
+def create_counter_4(): # і найкрутіший варіант за допомогою об'єкта-класу та замиканні
+    count = 0
+    class Increment:
+        def __call__(self): # для того, щоб екземпляри цього класу могли бути викликані як функції
+            nonlocal count
+            count += 1
+            print(count)
+            return self
+        def __repr__(self): # виводимо пустий рядок замість інформації про об'єкт
+            return ""
+    return Increment()
+
+counter_4 = create_counter_4()
+
+counter_4()()
