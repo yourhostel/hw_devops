fa6736b4867d6bbbcff37f71a8470a095e5343c7
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-03 02:02:54 +0200

    added attribute is final to Dog class

--- object-_oriented_programming/dog.py
+++ object-_oriented_programming/dog.py
@@ -2,9 +2,19 @@ class Dog:
     def __init__(self, color, eye_color, height, length, weight):
         self.color = color
         self.eye_color = eye_color
-        self.height = height
+        self.__height = height  # Використовуємо захищений атрибут (name mangling) _Dog__height
         self.length = length
         self.weight = weight
+
+    @property  # getter
+    def height(self):
+        return self.__height
+
+    @height.setter  # setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("Height cannot be negative")
+        self.__height = value
 
     def sit(self):
         print(f"{self.color} dog sits.")
