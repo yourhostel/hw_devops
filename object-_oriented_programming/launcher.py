963468a6c64e4c81a2281b4d528b493cbe594256
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-03 02:02:54 +0200

    added attribute is final to Dog class

--- object-_oriented_programming/launcher.py
+++ object-_oriented_programming/launcher.py
@@ -3,6 +3,24 @@ from dog import Dog
 rayne = Dog("Gray, White, and Black", "Blue and Brown", 18, 36, 30)
 
 rayne.sit()
+rayne.__height = 15
+print(rayne.__height)
 
-print(rayne.height)
+print(rayne._Dog__height)
 
+rayne._Dog__height = 7
+
+print(rayne._Dog__height)
+
+print("-------final-------")
+
+from final_attribute import Dog
+
+rayne = Dog(3)
+
+try:
+    rayne.age = 4
+except AttributeError as error:
+    print(f"Помилка: {error}")
+
+print(rayne.age)
