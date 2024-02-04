67eaf8ea6c528cc27b2d8e78b798d6fdd66fcb77
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-04 11:37:53 +0200

    corrected Dockerfile(3)

--- module_2/docker/run_nginx_docker.py
+++ module_2/docker/run_nginx_docker.py
@@ -1,17 +1,17 @@
 import docker
 
-# Створюємо клієнт Docker, використовуючи змінні оточення
+# Створюємо клієнта Docker, використовуючи змінні середовища
 client = docker.from_env()
 
 try:
-    # Запускаємо контейнер Nginx, відображаючи порт 80 контейнера на порт 8080 хоста
+    # Запускаємо контейнер Nginx, відображаючи порт 80 контейнера на порт 8081 хосту
     container = client.containers.run(
-        "my-custom-nginx",  # Використовуйте ім'я вашого нового образу
-        detach=True,  # Запуск в режимі фону
+        "my-custom-nginx",  # Ім'я вашого образу
+        detach=True,  # Запуск у фоновому режимі
         ports={"80/tcp": 8081},  # Відображення портів
+        name="my-nginx-container"  # Присвоюємо контейнеру ім'я для зручності
     )
     print(f"Контейнер Nginx запущено, ID: {container.id}")
-    print("Доступ до Nginx можна отримати за адресою http://localhost:8080")
+    print("Доступ до Nginx можна отримати за адресою http://localhost:8081")
 except Exception as e:
     print(f"Помилка при запуску контейнера Nginx: {e}")
-
