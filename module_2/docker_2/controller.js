d9112f4729205fdda7babf6a7cc26d2d7550c46f
Serhii Tyshchenko <yourhostel.ua@gmail.com>
2024-02-11 23:09:00 +0000

    Additional added

--- module_2/docker_2/controller.js
+++ module_2/docker_2/controller.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function createServer() {
     const app = express();
@@ -11,6 +12,12 @@ export default function createServer() {
 
     app.get('/healthz', (req, res) => {
         const data = { status: 'OK' };
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(data);
+    });
+
+    app.get('/uuid', (req, res) => {
+        const data = { uuid: uuidv4() };
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(data);
     });
