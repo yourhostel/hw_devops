7f7abaa2d5763e4bdc8aed67d9b40046b2b8372b
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-15 17:50:08 +0200

    change port 8080 -> 8081

--- module_2/docker_registry/controller.py
+++ module_2/docker_registry/controller.py
@@ -1,11 +1,14 @@
 from flask import jsonify
 import uuid
 
+
 def get_uuid():
     return jsonify(uuid=str(uuid.uuid4()))
 
+
 def get_home():
     return jsonify(message="Hello, World!")
+
 
 def get_healthz():
     return jsonify(status="OK")
