dda5c5d837ae01c515fcfd06e5b290dfdcff2c4a
Serhii Tyshchenko <yourhostel.ua@gmail.com>
2024-02-15 15:45:56 +0000

    docker_registry

---
+++ module_2/docker_registry/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+from controller import get_uuid, get_home, get_healthz
+
+app = Flask(__name__)
+
+app.add_url_rule('/', 'home', get_home)
+app.add_url_rule('/uuid', 'uuid', get_uuid)
+app.add_url_rule('/healthz', 'healthz', get_healthz)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
---
+++ module_2/docker_registry/controller.py
@@ -0,0 +1,11 @@
+from flask import jsonify
+import uuid
+
+def get_uuid():
+    return jsonify(uuid=str(uuid.uuid4()))
+
+def get_home():
+    return jsonify(message="Hello, World!")
+
+def get_healthz():
+    return jsonify(status="OK")
