d55ecd121c6d9327451eb2bebf331ad00e7d5412
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-20 03:21:18 +0200

    added scale_instance

---
+++ module_2/docker_compose_2/scale_instance/nginx/update_nginx.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Отримання IP адрес екземплярів сервісу app
+# shellcheck disable=SC2046
+APP_IPS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q --filter "name=app"))
+
+# Формування рядка серверів для конфігурації
+SERVERS=""
+for IP in $APP_IPS; do
+    SERVERS+="server $IP:80;\n"
+done
+
+# Заміна плейсхолдера у шаблоні конфігурації Nginx
+sed "s|{{app_servers}}|$SERVERS|" /path/to/nginx/default.conf.template > /path/to/nginx/default.conf
+
+# Перезапуск Nginx для нової конфігурації
+docker-compose restart nginx
