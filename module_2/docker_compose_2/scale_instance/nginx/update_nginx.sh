3735eda825dceda62a9543f2b7e8ac44d69c08a3
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-02-20 03:55:36 +0200

    added 'touch ./nginx/default.conf'

--- module_2/docker_compose_2/scale_instance/nginx/update_nginx.sh
+++ module_2/docker_compose_2/scale_instance/nginx/update_nginx.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-# Отримання IP адрес екземплярів сервісу app
+# Отримуємо IP адреси екземплярів сервісу app
 # shellcheck disable=SC2046
-APP_IPS=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q --filter "name=app"))
+APP_IPS=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $(docker ps -q --filter "name=app"))
 
-# Формування рядка серверів для конфігурації
+# Формуємо рядок серверів для конфігурації
 SERVERS=""
 for IP in $APP_IPS; do
-    SERVERS+="server $IP:80;\n"
+    SERVERS+="server $IP:80;"
 done
 
-# Заміна плейсхолдера у шаблоні конфігурації Nginx
-sed "s|{{app_servers}}|$SERVERS|" ./nginx/default.conf.template > ./nginx/default.conf
+# Створюємо файл, якщо він не існує
+touch ./nginx/default.conf
 
-# Перезапуск Nginx для нової конфігурації
+# Замінюємо плейсхолдер у шаблоні конфігурації Nginx і створюємо файл default.conf
+sed "s|{{app_servers}}|$SERVERS|" ./nginx/default.conf.template > ./nginx/default.conf.tmp && mv ./nginx/default.conf.tmp ./nginx/default.conf
+
+# Перезапускаємо Nginx для застосування нової конфігурації
 docker-compose restart nginx
