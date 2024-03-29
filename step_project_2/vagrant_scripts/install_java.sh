bc7c544f8ffc16944a0c5220c27e405ab0b4acaa
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-03-29 12:13:01 +0200

    added install_java.sh

---
+++ step_project_2/vagrant_scripts/install_java.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "Updating and installing OpenJDK 17"
+sudo apt-get update
+sudo apt-get install -y openjdk-17-jdk
+echo "export JAVA_HOME=$(readlink -f /usr/bin/java | sed 's:/bin/java::')" >> ~/.bashrc
+echo "export PATH=\$JAVA_HOME/bin:\$PATH" >> ~/.bashrc
