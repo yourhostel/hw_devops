b102f7d9fc1ea77c3cff4b56af2e7f44ca7ece75
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-14 00:24:03 +0300

    added egress

--- module_3/homework-terraform-create-instance/modules/security_group/main.tf
+++ module_3/homework-terraform-create-instance/modules/security_group/main.tf
@@ -2,7 +2,7 @@ resource "aws_security_group" "yourhostel_allow_ports" {
   vpc_id = var.vpc_id
   description = "Allows access from any location to the specified ports"
 
-  dynamic "ingress" {
+  dynamic "ingress" {   # Використання dynamic для for_each
     for_each = var.open_ports
     content {
       from_port   = ingress.value
@@ -12,6 +12,13 @@ resource "aws_security_group" "yourhostel_allow_ports" {
     }
   }
 
+  egress {    # Використання count для створення egress правил для прикладу
+    count       = length(var.open_ports)
+    from_port   = var.open_ports[count.index]
+    to_port     = var.open_ports[count.index]
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "yourhostel-open-ports"
   }
