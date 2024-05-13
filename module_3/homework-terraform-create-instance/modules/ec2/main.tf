21c9a4c22f4bf3dea94a9bab5a91cc866d210eb7
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-13 22:50:19 +0300

    change ami ec2 ami-05fd03138da450caf t3.micro

--- module_3/homework-terraform-create-instance/modules/ec2/main.tf
+++ module_3/homework-terraform-create-instance/modules/ec2/main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "yourhostel_web" {
-  ami           = "ami-0e342d72b12109f91" # AMI для Ubuntu з NGINX
-  instance_type = "t2.micro"
+  ami           = "ami-05fd03138da450caf"
+  instance_type = "t3.micro"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = [var.security_group_id]
   associate_public_ip_address = true
