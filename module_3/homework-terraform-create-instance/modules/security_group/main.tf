81885676cfd9d26172fd9e3cf490faf3534f9cdd
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-14 00:36:14 +0300

    added group_rule

--- module_3/homework-terraform-create-instance/modules/security_group/main.tf
+++ module_3/homework-terraform-create-instance/modules/security_group/main.tf
@@ -2,27 +2,29 @@ resource "aws_security_group" "yourhostel_allow_ports" {
   vpc_id      = var.vpc_id
   description = "Allows access from any location to the specified ports"
 
-  dynamic "ingress" {
-    # Використання dynamic для for_each
-    for_each = var.open_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-    }
-  }
-
-  egress {
-    # Використання count для створення egress правил для прикладу
-    count       = length(var.open_ports)
-    from_port   = var.open_ports[count.index]
-    to_port     = var.open_ports[count.index]
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
   tags = {
     Name = "yourhostel-open-ports"
   }
 }
+
+resource "aws_security_group_rule" "yourhostel_ingress_rules" {
+  count                    = length(var.open_ports)
+  type                     = "ingress"
+  from_port                = var.open_ports[count.index]
+  to_port                  = var.open_ports[count.index]
+  protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.yourhostel_allow_ports.id
+}
+
+resource "aws_security_group_rule" "yourhostel_egress_rules" {
+  count                    = length(var.open_ports)
+  type                     = "egress"
+  from_port                = var.open_ports[count.index]
+  to_port                  = var.open_ports[count.index]
+  protocol                 = "tcp"
+  cidr_blocks              = ["0.0.0.0/0"]
+  security_group_id        = aws_security_group.yourhostel_allow_ports.id
+}
+
 
