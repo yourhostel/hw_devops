8f678d4416ab5fc2529b02fde6c58dcb1233090b
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-13 23:17:49 +0300

    added default     = [80, 443, 22]

--- module_3/homework-terraform-create-instance/modules/security_group/variables.tf
+++ module_3/homework-terraform-create-instance/modules/security_group/variables.tf
@@ -6,4 +6,5 @@ variable "vpc_id" {
 variable "open_ports" {
   description = "List of ports to be opened"
   type        = list(number)
+  default     = [80, 443, 22]
 }
--- module_3/homework-terraform-create-instance/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "open_ports" {
-  description = "List of ports to be opened"
-  type        = list(number)
-}
