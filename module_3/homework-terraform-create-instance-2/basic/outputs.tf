6bdab5e93bffe0311dc1b6787f853dcc71add068
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-22 17:37:58 +0300

    change yourhostel_vpc

--- module_3/homework-terraform-create-instance-2/basic/outputs.tf
+++ module_3/homework-terraform-create-instance-2/basic/outputs.tf
@@ -1,13 +1,13 @@
 # outputs.tf
 
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  value = module.yourhostel_vpc.vpc_id
 }
 
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  value = module.yourhostel_vpc.public_subnets
 }
 
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value = module.yourhostel_vpc.private_subnets
 }
