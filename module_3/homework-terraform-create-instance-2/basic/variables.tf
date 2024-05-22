8c924642b38d0dcc33b0abcfa4bcb6e45772c4a9
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-22 18:41:10 +0300

    delete default variables

--- module_3/homework-terraform-create-instance-2/basic/variables.tf
+++ module_3/homework-terraform-create-instance-2/basic/variables.tf
@@ -3,7 +3,6 @@
 variable "name" {
   description = "Name of the VPC and related resources"
   type        = string
-  default      = "yurhostel"
 }
 
 variable "vpc_cidr" {
@@ -25,29 +24,24 @@ variable "public_subnets_cidrs" {
 variable "ami" {
   description = "AMI ID for the EC2 instances"
   type        = string
-  default     = "ami-05fd03138da450caf"
 }
 
 variable "instance_type" {
   description = "Instance type for the EC2 instances"
   type        = string
-  default     = "t3.micro"
 }
 
 variable "key_name" {
   description = "Key pair name for the EC2 instances"
   type        = string
-  default     = "YourHostelKey"
 }
 
 variable "region" {
   description = "AWS region"
   type        = string
-  default     = "eu-north-1"
 }
 
 variable "open_ports" {
   description = "List of ports to be opened"
   type        = list(number)
-  default     = [80, 443, 22]
 }
