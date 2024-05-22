a1d5eeac8a0bdbd250abdad5b9ab8e792b5e353d
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-22 17:25:12 +0300

    added yourhostel_security_group

--- module_3/homework-terraform-create-instance-2/basic/main.tf
+++ module_3/homework-terraform-create-instance-2/basic/main.tf
@@ -1,18 +1,26 @@
-module "vpc" {
-  source              = "./modules/VPC"
-  name                = var.name
-  vpc_cidr            = var.vpc_cidr
+module "yourhostel_vpc" {
+  source                = "./modules/VPC"
+  name                  = var.name
+  vpc_cidr              = var.vpc_cidr
   private_subnets_cidrs = var.private_subnets_cidrs
   public_subnets_cidrs  = var.public_subnets_cidrs
-  region              = var.region
+  security_group_id     = module.yourhostel_security_group.security_group_id
+  region                = var.region
+}
+
+module "yourhostel_security_group" {
+  source                = "./modules/security_group"
+  vpc_id                = module.yourhostel_vpc.vpc_id
+  open_ports            = var.open_ports
 }
 
 module "ec2" {
-  source              = "./modules/EC2"
-  name                = var.name
-  ami                 = var.ami
-  instance_type       = var.instance_type
-  public_subnet_id    = module.vpc.public_subnets[0]
-  private_subnet_id   = module.vpc.private_subnets[0]
-  key_name            = var.key_name
+  source                = "./modules/ec2"
+  name                  = var.name
+  ami                   = var.ami
+  instance_type         = var.instance_type
+  public_subnet_id      = module.yourhostel_vpc.public_subnets[0]
+  private_subnet_id     = module.yourhostel_vpc.private_subnets[0]
+  security_group_id     = module.yourhostel_security_group.security_group_id
+  key_name              = var.key_name
 }
