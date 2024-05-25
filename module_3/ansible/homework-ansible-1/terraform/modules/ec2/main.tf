f7c8ce6ea4ba1b3ee5dc11eef1c4558120b89500
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-05-25 14:21:42 +0300

    rename count to instance_count

--- module_3/ansible/homework-ansible-1/terraform/modules/ec2/main.tf
+++ module_3/ansible/homework-ansible-1/terraform/modules/ec2/main.tf
@@ -1,7 +1,7 @@
 # homework-ansible-1/terraform/ec2/main.tf
 
 resource "aws_instance" "this" {
-  count = var.count
+  count = var.instance_count
 
   ami                    = var.ami
   instance_type          = var.instance_type
