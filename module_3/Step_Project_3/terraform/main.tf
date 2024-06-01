0a5dc554f86724006f3f144ef7685ddea96eb1a0
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-06-01 21:59:04 +0300

    corrected terraform/main

--- module_3/Step_Project_3/terraform/main.tf
+++ module_3/Step_Project_3/terraform/main.tf
@@ -1,15 +1,39 @@
 # Step_Project_3/terraform/main.tf
 
-module "vpc" {
-  source = "./modules/vpc"
-  name   = var.name
+provider "aws" {
+  region = var.aws_region
+}
 
-  vpc_cidr            = var.vpc_cidr
-  vpc_azs             = var.vpc_azs
-  vpc_private_subnets = var.vpc_private_subnets
-  vpc_public_subnets  = var.vpc_public_subnets
-  enable_nat_gateway  = var.enable_nat_gateway
-  single_nat_gateway  = var.single_nat_gateway
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
+
+  name                 = var.name
+  cidr                 = var.vpc_cidr
+  azs                  = var.vpc_azs
+  private_subnets      = var.vpc_private_subnets
+  public_subnets       = var.vpc_public_subnets
+  enable_nat_gateway   = var.enable_nat_gateway
+  single_nat_gateway   = var.single_nat_gateway
+
+  tags = {
+    Name      = var.name
+    Owner     = var.name
+    CreatedBy = "${var.name}-automation"
+    Purpose   = "step3"
+  }
+  public_subnet_tags = {
+    Name      = "${var.name}-public"
+    Owner     = "${var.name}"
+    CreatedBy = "${var.name}-automation"
+    Purpose   = "step3"
+  }
+  private_subnet_tags = {
+    Name      = "${var.name}-private"
+    Owner     = "${var.name}"
+    CreatedBy = "${var.name}-automation"
+    Purpose   = "step3"
+  }
 }
 
 module "security_group" {
@@ -102,118 +126,3 @@ resource "local_file" "ansible_inventory" {
   })
   filename = "${path.module}/../ansible/inventory"
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#module "vpc" {
-#  source = "./modules/vpc"
-#  name   = var.name
-#}
-#
-#module "security_group" {
-#  source  = "./modules/security_group"
-#  name    = var.name
-#  vpc_id  = module.vpc.vpc_id
-#  open_ports = var.open_ports
-#}
-#
-#module "ec2" {
-#  source                 = "./modules/ec2"
-#  name                   = var.name
-#  ami                    = var.ami
-#  instance_type          = var.instance_type
-#  key_name               = var.key_name
-#  subnet_id              = module.vpc.public_subnet_id
-#  security_group_id      = module.security_group.security_group_id
-#  instance_count         = var.instance_count
-#}
-#
-#resource "local_file" "inventory" {
-#  content = templatefile("${path.module}/inventory.tpl", {
-#    instances = module.ec2.public_ips,
-#    instance_ids = module.ec2.instances,
-#    prometheus_port = var.prometheus_port,
-#    grafana_port = var.grafana_port,
-#    node_exporter_port = var.node_exporter_port,
-#    cadvisor_port = var.cadvisor_port,
-#    name = var.name,
-#    open_ports = var.open_ports
-#  })
-#  filename = "${path.module}/../ansible/inventory.ini"
-#}
-
-#resource "null_resource" "wait_for_instances" {
-#  provisioner "local-exec" {
-#    command = <<EOT
-#for ip in ${join(" ", module.ec2.public_ips)}; do
-#  for i in {1..10}; do
-#    nc -zv $ip 22 && break
-#    sleep 5
-#  done
-#done
-#EOT
-#  }
-#
-#  triggers = {
-#    instance_ids = join(",", module.ec2.instances)
-#  }
-#}
-#
-#resource "null_resource" "generate_ansible_hash" {
-#  provisioner "local-exec" {
-#    command = <<EOT
-#find ../ansible -type f -exec sha256sum {} \; | sort -k 2 | sha256sum > ../ansible/ansible_hash.txt
-#EOT
-#  }
-#
-#  triggers = {
-#    always_run = timestamp()
-#  }
-#}
-#
-#resource "null_resource" "run_ansible" {
-#  provisioner "local-exec" {
-#    command = "ANSIBLE_CONFIG=${path.module}/../ansible/ansible.cfg ansible-playbook ${path.module}/../ansible/playbooks/deploy.yml"
-#  }
-#
-#  depends_on = [
-#    null_resource.wait_for_instances,
-#    null_resource.generate_ansible_hash
-#  ]
-#
-#  triggers = {
-#    instance_ids       = join(",", module.ec2.instances)
-#    prometheus_port    = var.prometheus_port
-#    grafana_port       = var.grafana_port
-#    node_exporter_port = var.node_exporter_port
-#    cadvisor_port      = var.cadvisor_port
-#    open_ports         = join(",", var.open_ports)
-#    ansible_hash       = timestamp()
-#  }
-#}
--- module_3/Step_Project_3/terraform/modules/ec2/main.tf
+++ module_3/Step_Project_3/terraform/modules/ec2/main.tf
@@ -1,15 +1,16 @@
 # Step_Project_3/terraform/modules/ec2/main.tf
 
 resource "aws_instance" "this" {
-  count         = var.instance_count
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = element(var.subnet_ids, count.index)
-  security_groups = [var.security_group_id]
+  count = var.instance_count
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  vpc_security_group_ids = [var.security_group_id]
+  subnet_id              = element(var.subnet_ids, count.index)
 
   tags = {
-    Name      = "${var.name}-step-project-3-${count.index + 1}"
+    Name      = "${var.name}-ec2-${count.index + 1}"
     Owner     = var.name
     CreatedBy = "${var.name}-automation"
     Purpose   = "step3"
@@ -19,7 +20,8 @@ resource "aws_instance" "this" {
     inline = [
       "sudo apt-get update -y",
       "sudo apt-get install -y docker.io",
-      "sudo usermod -aG docker ${var.ansible_user}"
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker"
     ]
 
     connection {
@@ -30,6 +32,7 @@ resource "aws_instance" "this" {
     }
   }
 }
+
 
 
 
