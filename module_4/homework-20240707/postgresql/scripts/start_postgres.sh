1ee125c194667f6d39f050a698a28af4661ea430
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-07-26 16:41:29 +0300

    added start_postgres.sh

---
+++ module_4/homework-20240707/postgresql/scripts/start_postgres.sh
@@ -0,0 +1,9 @@
+# homework-20240707/postgresql/start_postgres.sh
+
+echo "Waiting for postgres_password..."
+while [ ! -f /vault/secrets/postgres_password ]; do
+  sleep 1
+done
+export POSTGRES_PASSWORD=$(cat /vault/secrets/postgres_password)
+echo "Found postgres_password, starting PostgreSQL..."
+exec docker-entrypoint.sh postgres
