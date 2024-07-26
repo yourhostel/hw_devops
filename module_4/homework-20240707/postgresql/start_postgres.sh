c7d2afc9524df024f2932ae235bdce93dd09861e
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-07-27 00:01:29 +0300

    added variant-2

--- module_4/homework-20240707/postgresql/start_postgres.sh
+++
@@ -1,9 +0,0 @@
-# homework-20240707/start_postgres.sh
-
-echo "Waiting for postgres_password..."
-while [ ! -f /vault/secrets/postgres_password ]; do
-  sleep 1
-done
-export POSTGRES_PASSWORD=$(cat /vault/secrets/postgres_password)
-echo "Found postgres_password, starting PostgreSQL..."
-exec docker-entrypoint.sh postgres
