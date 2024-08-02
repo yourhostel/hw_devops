a3064c046d991ef89282276c349f2d813c779b22
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-02 09:26:16 +0300

    output "load_balancer_ips"

--- module_4/homework-step-final/terraform/modules/ingress/main.tf
+++ module_4/homework-step-final/terraform/modules/ingress/main.tf
@@ -77,7 +77,7 @@ data "aws_network_interface" "elb_interface" {
 # Outputs
 output "load_balancer_ips" {
   description = "Public IPs associated with the Load Balancer"
-  value = data.aws_network_interfaces.elb_interfaces.ids
+  value = data.kubernetes_service.nginx_ingress_service.status.0.load_balancer.0.ingress[0].hostname
 #  value       = [for ni in data.aws_network_interfaces.elb_interfaces.ids : lookup(data.aws_network_interface.elb_interface[ni], "association", {})["public_ip"]]
 }
 
