5d82c2996afb478eea561d3dd177159e8baf9c1e
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-02 07:12:50 +0300

    added VPC corrected 2

--- module_4/homework-step-final/terraform/modules/vpc/modules/vpc/main.tf
+++ module_4/homework-step-final/terraform/modules/vpc/modules/vpc/main.tf
@@ -1,5 +1,13 @@
 # modules/vpc/main.tf
 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr_block
   tags = merge(
