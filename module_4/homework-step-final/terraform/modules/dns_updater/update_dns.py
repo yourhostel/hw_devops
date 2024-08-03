407b2f66919738bfd2a44ad7c03b4ba3446a5337
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-03 08:34:43 +0300

    finish update_dns.py

--- module_4/homework-step-final/terraform/modules/dns_updater/update_dns.py
+++ module_4/homework-step-final/terraform/modules/dns_updater/update_dns.py
@@ -5,47 +5,6 @@ import json
 import requests
 
 input_data = json.load(sys.stdin)
-# print(json.dumps(input_data))
-# # Extracting parameters for the request
-# url = input_data["url"]
-# auth_token = input_data["auth_token"]
-# data_param = input_data["data"]
-# dns_record_id = input_data["subdomain_id"]
-# priority = input_data["priority"]
-#
-# # Setting up headers and data for the POST request
-# headers = {
-#     "Authorization": "Bearer " + auth_token,
-#     "Content-Type": "application/x-www-form-urlencoded"
-# }
-# data = {
-#     "data": data_param,
-#     "subdomain_id": subdomain_id,
-#     "priority": priority
-# }
-#
-# response = requests.post(url, headers=headers, data=data).json()
-#
-#
-# # Recursive processing of the response object from the hosting provider
-# def flatten_to_string(obj, parent_key='', sep='_'):
-#     items = {}
-#     if isinstance(obj, dict):
-#         for k, v in obj.items():
-#             new_key = f"{parent_key}{sep}{k}" if parent_key else str(k)
-#             items.update(flatten_to_string(v, new_key, sep=sep))
-#     elif isinstance(obj, list):
-#         for i, item in enumerate(obj):
-#             new_key = f"{parent_key}{sep}{i}" if parent_key else str(i)
-#             items.update(flatten_to_string(item, new_key, sep=sep))
-#     else:
-#         items[parent_key] = str(obj)
-#     return items
-#
-#
-# flattened_response = flatten_to_string(response)
-# print(json.dumps(flattened_response, ensure_ascii=False))
-
 
 # Extracting parameters for the request
 url = input_data["url"]
