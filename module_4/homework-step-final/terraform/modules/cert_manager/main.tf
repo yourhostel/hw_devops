0306857d2dadc93ca811d04c11d559e2ca962172
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-05 01:42:55 +0300

    debugging terraform/modules/cert_manager/main.tf

--- module_4/homework-step-final/terraform/modules/cert_manager/main.tf
+++ module_4/homework-step-final/terraform/modules/cert_manager/main.tf
@@ -27,33 +27,63 @@ resource "helm_release" "cert_manager" {
   }
 }
 
-resource "kubernetes_manifest" "cluster_issuer" {
+resource "null_resource" "cluster_issuer" {
   depends_on = [helm_release.cert_manager]
 
-  manifest = {
-    apiVersion = "cert-manager.io/v1"
-    kind       = "ClusterIssuer"
-    metadata   = {
-      name = "letsencrypt-prod"
-    }
-    spec = {
-      acme = {
-        server = "https://acme-v02.api.letsencrypt.org/directory"
-        email  = "youremail@example.com"
-        privateKeySecretRef = {
-          name = "letsencrypt-prod"
-        }
-        solvers = [{
-          http01 = {
-            ingress = {
-              class = "nginx"
-            }
-          }
-        }]
-      }
-    }
+  provisioner "local-exec" {
+    command = <<EOT
+kubectl apply -f - <<EOF
+apiVersion: cert-manager.io/v1
+kind: ClusterIssuer
+metadata:
+  name: letsencrypt-prod
+spec:
+  acme:
+    server: https://acme-v02.api.letsencrypt.org/directory
+    email: youremail@example.com
+    privateKeySecretRef:
+      name: letsencrypt-prod
+    solvers:
+    - http01:
+        ingress:
+          class: nginx
+EOF
+EOT
+  }
+
+  triggers = {
+    always_run = "${timestamp()}"
   }
 }
+
+
+#resource "kubernetes_manifest" "cluster_issuer" {
+#  depends_on = [helm_release.cert_manager]
+#
+#  manifest = {
+#    apiVersion = "cert-manager.io/v1"
+#    kind       = "ClusterIssuer"
+#    metadata   = {
+#      name = "letsencrypt-prod"
+#    }
+#    spec = {
+#      acme = {
+#        server = "https://acme-v02.api.letsencrypt.org/directory"
+#        email  = "youremail@example.com"
+#        privateKeySecretRef = {
+#          name = "letsencrypt-prod"
+#        }
+#        solvers = [{
+#          http01 = {
+#            ingress = {
+#              class = "nginx"
+#            }
+#          }
+#        }]
+#      }
+#    }
+#  }
+#}
 
 
 
