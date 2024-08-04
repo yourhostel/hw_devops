0cd6d942251e91ae5e3aa9a80a2f60acc94bd385
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-04 22:58:46 +0300

    explicit declaration helm providers

--- module_4/homework-step-final/terraform/modules/cert_manager/main.tf
+++ module_4/homework-step-final/terraform/modules/cert_manager/main.tf
@@ -3,7 +3,12 @@
 terraform {
   required_providers {
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
     }
   }
 }
