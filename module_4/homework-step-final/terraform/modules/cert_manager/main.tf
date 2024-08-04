332f6e125cea7a32cfa78f2067fe4e9673f14e3d
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-05 00:28:38 +0300

    added clusterissuer.yaml and certificate.yaml

--- module_4/homework-step-final/terraform/modules/cert_manager/main.tf
+++ module_4/homework-step-final/terraform/modules/cert_manager/main.tf
@@ -1,17 +1,17 @@
 # terraform/modules/cert_manager/main.tf
 
-#terraform {
-#  required_providers {
-#    kubernetes = {
-#      source  = "hashicorp/kubernetes"
-#      version = "~> 2.0"
-#    }
-#    helm = {
-#      source  = "hashicorp/helm"
-#      version = "~> 2.0"
-#    }
-#  }
-#}
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
+  }
+}
 
 # Installing Cert Manager
 resource "helm_release" "cert_manager" {
@@ -29,48 +29,59 @@ resource "helm_release" "cert_manager" {
 
 # Creating ClusterIssuer
 resource "kubernetes_manifest" "cluster_issuer" {
-  manifest = {
-    apiVersion = "cert-manager.io/v1"
-    kind       = "ClusterIssuer"
-    metadata   = {
-      name = "letsencrypt-prod"
-    }
-    spec = {
-      acme = {
-        server          = "https://acme-v02.api.letsencrypt.org/directory"
-        email           = "yourhostel.ua@gmail.com"
-        privateKeySecretRef = {
-          name = "letsencrypt-prod"
-        }
-        solvers = [{
-          http01 = {
-            ingress = {
-              class = "nginx"
-            }
-          }
-        }]
-      }
-    }
-  }
+  manifest = file("clusterissuer.yaml")
 }
 
 # Requesting a certificate for the domain final.tyshchenko.online
 resource "kubernetes_manifest" "certificate" {
-  manifest = {
-    apiVersion = "cert-manager.io/v1"
-    kind       = "Certificate"
-    metadata   = {
-      name      = "final-tyshchenko-online"
-      namespace = "cert-manager"
-    }
-    spec = {
-      secretName = "final-tyshchenko-online-tls"
-      issuerRef = {
-        name = "letsencrypt-prod"
-        kind = "ClusterIssuer"
-      }
-      dnsNames = ["final.tyshchenko.online"]
-    }
-  }
+  manifest = file("certificate.yaml")
 }
+
+## Creating ClusterIssuer
+#resource "kubernetes_manifest" "cluster_issuer" {
+#  manifest = {
+#    apiVersion = "cert-manager.io/v1"
+#    kind       = "ClusterIssuer"
+#    metadata   = {
+#      name = "letsencrypt-prod"
+#    }
+#    spec = {
+#      acme = {
+#        server          = "https://acme-v02.api.letsencrypt.org/directory"
+#        email           = "yourhostel.ua@gmail.com"
+#        privateKeySecretRef = {
+#          name = "letsencrypt-prod"
+#        }
+#        solvers = [{
+#          http01 = {
+#            ingress = {
+#              class = "nginx"
+#            }
+#          }
+#        }]
+#      }
+#    }
+#  }
+#}
+#
+## Requesting a certificate for the domain final.tyshchenko.online
+#resource "kubernetes_manifest" "certificate" {
+#  manifest = {
+#    apiVersion = "cert-manager.io/v1"
+#    kind       = "Certificate"
+#    metadata   = {
+#      name      = "final-tyshchenko-online"
+#      namespace = "cert-manager"
+#    }
+#    spec = {
+#      secretName = "final-tyshchenko-online-tls"
+#      issuerRef = {
+#        name = "letsencrypt-prod"
+#        kind = "ClusterIssuer"
+#      }
+#      dnsNames = ["final.tyshchenko.online"]
+#    }
+#  }
+#}
+
 
