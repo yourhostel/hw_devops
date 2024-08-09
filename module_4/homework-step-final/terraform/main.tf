0f87d7539072a99dd5a3543b5104cd6447e38c24
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-09 20:50:35 +0300

    close module.argo_cd.argo_cd_admin_password

--- module_4/homework-step-final/terraform/main.tf
+++ module_4/homework-step-final/terraform/main.tf
@@ -130,10 +130,10 @@ output "ingress_nginx_controller" {
   value = module.ingress.ingress_nginx_controller
 }
 
-output "argo_cd_admin_password" {
-  value = module.argo_cd.argo_cd_admin_password
-  sensitive = true
-}
+#output "argo_cd_admin_password" {
+#  value = module.argo_cd.argo_cd_admin_password
+#  sensitive = true
+#}
 
 # Output of nginx_ingress_service object for debugging
 #output "nginx_ingress_service_full" {
