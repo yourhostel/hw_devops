c0b25ee2dd3ac6dfea9cd8f5e214e9e8978844bb
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-09 10:12:58 +0300

    force-ssl-redirect" = "false"

--- module_4/homework-step-final/terraform/modules/argo_cd/main.tf
+++ module_4/homework-step-final/terraform/modules/argo_cd/main.tf
@@ -29,13 +29,5 @@ resource "helm_release" "argo_cd" {
   chart      = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   version    = "5.34.2"
-
-  values = [
-  <<EOF
-server:
-  extraArgs:
-    - --insecure
-EOF
-]
 }
 
