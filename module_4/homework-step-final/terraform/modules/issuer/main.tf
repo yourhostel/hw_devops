2da36e0a16b225199c5db72f4597b37372707a05
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-09 11:50:12 +0300

    force-ssl-redirect" = "false"

--- module_4/homework-step-final/terraform/modules/issuer/main.tf
+++ module_4/homework-step-final/terraform/modules/issuer/main.tf
@@ -55,6 +55,8 @@ resource "kubernetes_manifest" "https_ingress" {
         "cert-manager.io/cluster-issuer" = "letsencrypt-prod"
         "nginx.ingress.kubernetes.io/ssl-redirect" = "false"
         "nginx.ingress.kubernetes.io/force-ssl-redirect" = "false"
+        "nginx.ingress.kubernetes.io/use-regex" = "true"
+        "nginx.ingress.kubernetes.io/backend-protocol" = "HTTP"
       }
     }
     spec = {
