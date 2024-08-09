e8e8b46a1c761f0fd4dca4dcb4283383128b7c24
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-09 08:02:08 +0300

    force-ssl-redirect" = "false"

--- module_4/homework-step-final/terraform/modules/argo_cd/main.tf
+++ module_4/homework-step-final/terraform/modules/argo_cd/main.tf
@@ -41,17 +41,3 @@ EOF
   ]
 }
 
-# Fetch the initial admin password for Argo CD
-data "kubernetes_secret" "argocd_initial_admin_secret" {
-  depends_on = [helm_release.argo_cd]
-
-  metadata {
-    name      = "argocd-initial-admin-secret"
-    namespace = "argocd"
-  }
-}
-
-output "argo_cd_initial_admin_password" {
-  value = data.kubernetes_secret.argocd_initial_admin_secret.data["password"]
-#  sensitive = true
-}
