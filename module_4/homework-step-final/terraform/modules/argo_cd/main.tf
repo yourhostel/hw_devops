e791bb6967df2743e617feab38a62bb38290d4f1
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-10 20:27:09 +0300

    argo-cd-argocd-server insecure = "true" 2

--- module_4/homework-step-final/terraform/modules/argo_cd/main.tf
+++ module_4/homework-step-final/terraform/modules/argo_cd/main.tf
@@ -25,14 +25,11 @@ resource "helm_release" "argo_cd" {
     name  = "server.service.type"
     value = "ClusterIP"
   }
-#  set {
-#    name  = "server.extraArgs"
-#    value = "{--insecure=true}"
-#  }
   set {
     name  = "server.extraArgs"
-    value = "{--insecure=true,--disable-auth=true,--disable-auth-for-local-ips=true}"
+    value = "{--insecure=true}"
   }
+
 }
 
 data "kubernetes_secret" "argocd_initial_admin_secret" {
