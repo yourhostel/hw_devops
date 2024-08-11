80c88a7afc45ccf394c756488298dc2cd120af81
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-11 04:33:01 +0300

    corrected helm chart static_site

--- module_4/homework-step-final/terraform/modules/static_site/main.tf
+++ module_4/homework-step-final/terraform/modules/static_site/main.tf
@@ -1,92 +1,29 @@
 # terraform/modules/static_site/main.tf
 
-resource "kubernetes_config_map" "nginx_html" {
-  metadata {
-    name      = "nginx-html"
-    namespace = "argocd"
+resource "helm_release" "static_site" {
+  name       = "static-site"
+  namespace  = "argocd"
+  repository = "https://github.com/yourhostel/hw_devops"
+  chart      = "module_4/homework-step-final/helm_charts/static-site"
+  version    = "main"
+
+  set {
+    name  = "title"
+    value = "Welcome to final.tyshchenko.online"
   }
 
-  data = {
-    "index.html" = <<-EOF
-<html>
-<head><title>Welcome</title></head>
-<body>
-<h1>Welcome to final.tyshchenko.online</h1>
-</body>
-</html>
-EOF
-  }
-}
-
-resource "kubernetes_deployment" "static_site" {
-  metadata {
-    name      = "static-site"
-    namespace = "argocd"
+  set {
+    name  = "message"
+    value = "This is a static site served by Nginx"
   }
 
-  spec {
-    replicas = 1
-    selector {
-      match_labels = {
-        app = "static-site"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "static-site"
-        }
-      }
-
-      spec {
-        container {
-          name  = "nginx"
-          image = "nginx:latest"
-
-          port {
-            container_port = 80
-          }
-
-          volume_mount {
-            name       = "html-volume"
-            mount_path = "/usr/share/nginx/html"
-            read_only  = true
-          }
-        }
-
-        volume {
-          name = "html-volume"
-
-          config_map {
-            name = kubernetes_config_map.nginx_html.metadata[0].name
-            items {
-              key  = "index.html"
-              path = "index.html"
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-resource "kubernetes_service" "static_site" {
-  metadata {
-    name      = "static-site-service"
-    namespace = "argocd"
+  set {
+    name  = "replicas"
+    value = 1
   }
 
-  spec {
-    selector = {
-      app = "static-site"
-    }
-
-    port {
-      port        = 80
-      target_port = 80
-    }
-
-    type = "ClusterIP"
+  set {
+    name  = "nginx.tag"
+    value = "latest"
   }
 }
