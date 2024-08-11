e9bad031f413678bd117e4a9c6305c07335290a0
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-11 07:09:39 +0300

    added flask and index.html to build image python-app

--- module_4/homework-step-final/python/app.py
+++ module_4/homework-step-final/python/app.py
@@ -1,20 +1,16 @@
 # python/app.py
 
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
 
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-
-
-def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler):
-    server_address = ('', 8080)
-    httpd = server_class(server_address, handler_class)
-    print("Server started at localhost:8080")
-    httpd.serve_forever()
+@app.route('/')
+def index():
+    with open('index.html', 'r') as file:
+        html_content = file.read()
+    return render_template_string(html_content)
 
 
 if __name__ == '__main__':
-    run()
+    app.run(host='0.0.0.0', port=8080)
