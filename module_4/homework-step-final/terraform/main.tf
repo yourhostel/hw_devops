461570a1d7eed14a213ad1359ebcca75917d133c
Serhii Tishchenko <yourhostel.ua@gmail.com>
2024-08-14 05:17:44 +0300

    open argo_application

--- module_4/homework-step-final/terraform/main.tf
+++ module_4/homework-step-final/terraform/main.tf
@@ -45,11 +45,11 @@ module "cert_manager" {
 module "static_site" {
   source = "./modules/static_site"
 }
-#
-#module "python_app" {
-#  source = "./modules/python_app"
-#}
-#
+
+module "python_app" {
+  source = "./modules/python_app"
+}
+
 module "issuer" {
   source = "./modules/issuer"
   depends_on = [module.cert_manager]
@@ -69,20 +69,20 @@ module "argo_cd" {
     helm       = helm
   }
 }
-#
-#module "argo_application" {
-#  source = "./modules/argo_application"
-#  argocd_ready = module.argo_cd.argocd_ready
-#  depends_on = [
-#    module.cert_manager,
-#    module.ingress,
-#  ]
-#
-#  providers = {
-#    kubernetes = kubernetes
-#    helm       = helm
-#  }
-#}
+
+module "argo_application" {
+  source = "./modules/argo_application"
+  argocd_ready = module.argo_cd.argocd_ready
+  depends_on = [
+    module.cert_manager,
+    module.ingress,
+  ]
+
+  providers = {
+    kubernetes = kubernetes
+    helm       = helm
+  }
+}
 
 module "ingress" {
   source = "./modules/ingress"
